645152d63230deaae1b83c4949b0a04837b08db4
Azikenp <azikenp26@gmail.com>
2023-08-26 20:55:24 +0100

    adding load options to the paginate react package

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8d5d2a2cd3msha12dddadeb22130p1b8b49jsnc231130cff9f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
